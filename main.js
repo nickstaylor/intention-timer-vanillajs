3f1fbc7298ae61427ff89a2f7ea0c6b5c06a566c
Nick Taylor <nickstaylor@gmail.com>
2021-07-31 12:07:11 -0600

    Add functions for displaying activity information on timer page

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//form section query selectors
 var buttonSection = document.querySelector(".top-buttons")
 var categoryButtons = document.querySelectorAll(".categories")
 var errorMessages = document.querySelectorAll(".error-message")
@@ -5,8 +6,14 @@ var startButton = document.querySelector("#start-activity");
 var taskInput = document.querySelector("#accomplish");
 var minutesInput = document.querySelector("#minutes-input")
 var secondsInput = document.querySelector("#seconds-input")
-var timerPage = document.querySelector(".timer-section")
 var formPage = document.querySelector(".user-form")
+var leftSideHeader = document.querySelector(".left-side-header")
+//timer section query selectors
+var timerPage = document.querySelector(".timer-section")
+var activityName = document.querySelector(".activity-name")
+var timerDisplay = document.querySelector(".timer-display")
+var timerButton = document.querySelector(".timer-circle-btn")
+var logActivityButton = document.querySelector(".log-activity-btn")
 
 
 //global variables
@@ -14,15 +21,18 @@ var chosenCategory = '';
 var task = '';
 var minutes = '';
 var seconds = '';
+var currentActivity = {};
 var timer = null;
 
 //event listeners
-startButton.addEventListener('click', startActivity)
+startButton.addEventListener('click', validateCreateActivity)
 buttonSection.addEventListener('click', selectCategory)
 minutesInput.addEventListener('keyup', validateMinSec)
 secondsInput.addEventListener('keyup', validateMinSec)
+timerButton.addEventListener('click', startTimer)
+logActivityButton.addEventListener('click', logActivity)
 
-
+////// FORM VALIDATION and ACTIVITY CREATION //////
 function selectCategory(e) {
   let activity = e.target.dataset.id
   chosenCategory = activity
@@ -47,7 +57,7 @@ function validateMinSec() {
   secondsInput.value = secondsInput.value.replace(/[^0-9]/, "");
 }
 
-function checkInputs() {
+function validateInputs() {
   task = taskInput.value
   minutes = parseInt(minutesInput.value)
   seconds = parseInt(secondsInput.value)
@@ -85,11 +95,36 @@ function displayHideErrorMessages(inputsObject) {
   return errors
 }
 
-function startActivity(e) {
+function validateCreateActivity(e) {
   e.preventDefault();
-  let validated = checkInputs();
-  if (validated.validTime) {
-    formPage.classList.add('hidden');
-    timerPage.classList.remove('hidden');
+  let validatedActivity = validateInputs();
+
+  if (validatedActivity.validTime) {
+    delete validatedActivity.validTime
+    currentActivity = new Activity(validatedActivity)
+    displayTimerPage();
   }
 }
+////// end of FORM VALIDATION and ACTIVITY CREATION //////
+
+////// TIMER FUNCTIONS and ACTIVITY LOGGING //////
+
+function displayTimerPage() {
+  let activity = currentActivity
+  formPage.classList.add('hidden');
+  leftSideHeader.innerHTML = `Current Activity : <span class="${activity.category}-active">${activity.category}</span>`
+  timerButton.classList.add(`${activity.category}-border`)
+  activityName.innerText = `${activity.description}`
+  timerDisplay.innerText = `${activity.minutes < 10 ? "0" + `${activity.minutes}` : `${activity.minutes}`}:${activity.seconds < 10 ? "0" + `${activity.seconds}` : `${activity.seconds}`}`
+  timerPage.classList.remove('hidden');
+}
+
+function startTimer() {
+  console.log('currentActivity', currentActivity)
+}
+
+function logActivity() {
+  
+}
+
+////// end of TIMER FUNCTIONS and ACTIVITY LOGGING //////
