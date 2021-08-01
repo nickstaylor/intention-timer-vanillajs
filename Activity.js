1ad68a8934ee0aee2d40f2e7613f532acd16aab9
Nick Taylor <nickstaylor@gmail.com>
2021-08-01 09:20:52 -0600

    Timer is fully functioning with ability to pause.  Also added markComlete method to reset totalTime and mark activity complete

--- Activity.js
+++ Activity.js
@@ -6,16 +6,52 @@ class Activity {
     this.seconds = seconds;
     this.completed = false;
     this.id = Date.now();
+    this.totalTime = (this.minutes * 60) + this.seconds;
+    this.timerActive = false
   }
+  
 
-  countDown() {
-    //timer logic here
+  startTimer({ timerDisplay, timerButton, logActivityButton }) {
+    let countDown = () => {
+      if (!this.timerActive) {
+        clearInterval(timer)
+        return
+      }
+      let minutes = Math.floor(this.totalTime / 60)
+      let seconds = (this.totalTime-1) % 60
+      if (minutes && seconds === 59) {
+        minutes = minutes - 1
+      }
+       if (minutes < 10) {
+         minutes = "0" + minutes;
+       }
+      if (seconds < 10) {
+        seconds = "0" + seconds;
+      }
+
+      timerDisplay.innerText = `${minutes}:${seconds}`
+      this.totalTime--
+      if (this.totalTime < 1) {
+        clearInterval(timer)
+        this.markComplete(timerButton, logActivityButton)
+      }
+    }
+
+    let timer = null;
+    console.log('this.timerActive', this.timerActive)
+    if (this.timerActive) {
+      timer = setInterval(countDown, 1000)
+      timerButton.innerText = "pause"
+    } 
   }
-
-  markComplete() {
-    this.complete = true;
-    //insert adjcacent html for 'create a new activity'
-    //inset adjacent html logic here for each activity
+  
+  
+  markComplete(timerButton) {
+    this.completed = true;
+    this.timerActive = false;
+    this.totalTime = this.minutes * 60 + this.seconds;
+    timerButton.innerText = "congrats!"
+    logActivityButton.classList.remove('hidden')
   }
 
   saveToStorage() {
