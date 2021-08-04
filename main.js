398928104383db35127cad69a9ffdece72619379
Nick Taylor <nickstaylor@gmail.com>
2021-08-04 16:22:31 -0600

    Add functions for disabling favorite and replay buttons while timer is active.  Also added hover functionality for deleting

--- main.js
+++ main.js
@@ -23,6 +23,7 @@ var savedActivitiesSection = document.querySelector(".activities-section")
 var showFavorites = document.querySelector(".show-favorites")
 
 
+
 //global variables
 var chosenCategory = '';
 var task = '';
@@ -32,6 +33,7 @@ var currentActivity = {};
 var savedActivities = [];
 var favorites = [];
 var showingFavorites = false;
+let activeDelay;
 
 //event listeners
 startButton.addEventListener('click', validateCreateActivity)
@@ -150,26 +152,27 @@ function validateCreateActivity(e) {
 ////// end of FORM VALIDATION and ACTIVITY CREATION //////
 
 ////// TIMER FUNCTIONS and ACTIVITY LOGGING //////
-
 function displayTimerPage() {
-  let activity = currentActivity
-  formPage.classList.add('hidden');
-  leftSideHeader.innerHTML = `Current Activity : <span class="${activity.category}-active">${activity.category}</span>`
-  timerButton.classList.add(`${activity.category}-border`)
-  activityName.innerText = `${activity.description}`
-  timerDisplay.innerText = `${activity.minutes < 10 ? "0" + `${activity.minutes}` : `${activity.minutes}`}:${activity.seconds < 10 ? "0" + `${activity.seconds}` : `${activity.seconds}`}`
-  timerPage.classList.remove('hidden');
+  let activity = currentActivity;
+  formPage.classList.add("hidden");
+  leftSideHeader.innerHTML = `Current Activity : <span class="${activity.category}-active">${activity.category}</span>`;
+  timerButton.classList.add(`${activity.category}-border`);
+  activityName.innerText = `${activity.description}`;
+  timerDisplay.innerText = `${activity.minutes < 10 ? "0" + `${activity.minutes}` : `${activity.minutes}`}:${activity.seconds < 10 ? "0" + `${activity.seconds}` : `${activity.seconds}`}`;
+  timerPage.classList.remove("hidden");
 }
 
 function startTimer() {
   if (timerButton.innerText === 'PAUSE') {
     currentActivity.timerActive = false
     timerButton.innerText = 'start'
-    currentActivity.startTimer({timerDisplay, timerButton, logActivityButton});
+    currentActivity.startTimer({ timerDisplay, timerButton, logActivityButton });
+    toggleReplayFavoriteButtons(currentActivity.timerActive);
     return
   }
-    currentActivity.timerActive = true;
-    currentActivity.startTimer({timerDisplay, timerButton, logActivityButton});
+  currentActivity.timerActive = true;
+  currentActivity.startTimer({ timerDisplay, timerButton, logActivityButton });
+  toggleReplayFavoriteButtons(currentActivity.timerActive)
 }
 
 
@@ -182,30 +185,72 @@ function logActivity() {
   if (!showingFavorites) {
     insertSavedActivity(currentActivity)
   }
+  toggleReplayFavoriteButtons(currentActivity.timerActive)
+}
+
+function showDelete(id) {
+  let deleteSection = document.querySelector(`.delete-section-${id}`);
+  let blackOutSection = document.querySelector(`.body-blackout-${id}`);
+    console.log('show delete')
+    activeDelay = setTimeout(function (id) {
+          deleteSection.classList.remove("hidden");
+          blackOutSection.classList.remove("hidden");
+    }, 2500)
+
+  }
+  
+function hideDelete(id) {
+  clearTimeout(activeDelay)
+  console.log('clear')
+  let deleteSection = document.querySelector(`.delete-section-${id}`)
+  let blackOutSection = document.querySelector(`.body-blackout-${id}`)
+  deleteSection.classList.add('hidden')
+  blackOutSection.classList.add('hidden')
+}
+
+
+function deleteActivity(id) {
+ console.log(id)
 }
 
 function insertSavedActivity(activity) {
   noActivityMessage.classList.add('hidden')
   savedActivitiesSection.insertAdjacentHTML(
     "afterbegin",
-      `
-      <div class="activity-container" data-id=${activity.id}>
-        <section class="top-of-activity-card activity-card-${activity.category}">
-          <p class="activity-card-category">${activity.category}</p>
-          <p class="activity-card-time">${activity.minutes} minutes ${activity.seconds} Seconds</p>
-        </section>
-        <section class="botton-of-activity-card">
-          <p class="activity-card-desc">${activity.description}</p>
-          <div class="replay-favorite-images">
-          <img src="assets/replay-hollow.svg" class="replay-img" onclick="replayActivity(${activity.id})" />
-          <img src="assets/heart-${activity.favorite ? 'filled.svg' : 'outline.svg'}" class="favorite-img" id="${activity.id}" onclick="favoriteActivity(${activity.id})" />
-          </div>
-        </section>
-      </div>
-      `
+    `
+    <div class="activity-container activity-${activity.id}" onmouseenter="showDelete(${activity.id}, true)" onmouseleave="hideDelete(${activity.id}, false)" data-id=${
+      activity.id
+    }>
+      <section class="delete-section delete-section-${activity.id} hidden">
+        <p class="delete-activity" onclick="deleteActivity(${
+          activity.id
+        })" >Delete Activity?</>
+      </section>
+      <div class="body-blackout body-blackout-${activity.id} hidden"></div>
+      <section class="top-of-activity-card activity-card-${activity.category}">
+        <p class="activity-card-category">${activity.category}</p>
+        <p class="activity-card-time">${activity.minutes} minutes ${
+      activity.seconds
+    } Seconds</p>
+      </section>
+      <section class="botton-of-activity-card">
+        <p class="activity-card-desc">${activity.description}</p>
+        <div class="replay-favorite-images">
+          <img src="assets/replay-hollow.svg" class="replay-img" onclick="replayActivity(${
+            activity.id
+          })" />
+          <img src="assets/heart-${
+            activity.favorite ? "filled.svg" : "outline.svg"
+          }" class="favorite-img" id="${
+      activity.id
+    }" onclick="favoriteActivity(${activity.id})" />
+        </div>
+      </section>
+    </div>
+    `
   );
-}
-
+  }
+  
 function displayFavorites() {
   showingFavorites = !showingFavorites
   if (showingFavorites) {
@@ -226,35 +271,52 @@ function displayFavoritesOnCard() {
       insertSavedActivity(activity)
     }
   })
-}
-
-function replayActivity(id) {
-  console.log('id replay', id)
-}
-
-function favoriteActivity(id) {
-  let alreadyFavorite = alreadyFavorited(id)
-
-  let favorite = savedActivities.find(act => id === act.id)
-  var activityCards = Array.from(document.querySelectorAll('.activity-container'));
-  activityCards.forEach(card => {
-    if (parseInt(card.dataset.id) === id) {
-      let image = card.querySelector(".favorite-img")
-      if (alreadyFavorite) {
-        image.src = 'assets/heart-outline.svg'
-        favorite.favorite = false;
-      } else {
-        image.src = 'assets/heart-filled.svg'
-        favorite.favorite = true;
-      }
-    }
-  })
-  favorite.saveToStorage(savedActivities)
-  if (showingFavorites) {
-    displayFavoritesOnCard()
   }
+  
+function replayActivity(id) {
+  let activity = savedActivities.find(act => id === act.id)
+  console.log('activity', activity)
 }
 
+function toggleReplayFavoriteButtons(timerActive) {
+  var replayFavoriteImages = Array.from(
+    document.querySelectorAll(".replay-favorite-images")
+  );
+  replayFavoriteImages.push(showFavorites)
+
+  replayFavoriteImages.forEach(image => {
+    if (timerActive) {
+      image.classList.add('pointer-events')
+    } else {
+      image.classList.remove('pointer-events')
+    }
+    })
+  }
+  
+  
+  function favoriteActivity(id) {
+    let alreadyFavorite = alreadyFavorited(id)
+    
+    let favorite = savedActivities.find(act => id === act.id)
+    var activityCards = Array.from(document.querySelectorAll('.activity-container'));
+    activityCards.forEach(card => {
+      if (parseInt(card.dataset.id) === id) {
+        let image = card.querySelector(".favorite-img")
+        if (alreadyFavorite) {
+          image.src = 'assets/heart-outline.svg'
+          favorite.favorite = false;
+        } else {
+          image.src = 'assets/heart-filled.svg'
+          favorite.favorite = true;
+        }
+      }
+    })
+    favorite.saveToStorage(savedActivities)
+    if (showingFavorites) {
+      displayFavoritesOnCard()
+    }
+  }
+    
 function alreadyFavorited(id) {
   let alreadyFavorited = favorites.find(fav => id === fav)
   if (!alreadyFavorited) {
@@ -282,7 +344,7 @@ function resetForm() {
   timerPage.classList.add("hidden");
   formPage.classList.remove("hidden");
 }
-
+    
 function resetGlobalVariables() {
   currentActivity = {};
   chosenCategory = '';
@@ -310,5 +372,6 @@ function resetCategoryButtons(chosenCategory) {
     }
   });
 }
-
-////// end of REST FUNCTIONS //////
+    
+////// end of RESET FUNCTIONS //////
+    
